5e20b315fcd9e40245d93d0c26fac8566c760a87
Nir Moav <nirmoav@fb.com>
2020-10-07 22:25:25 +0300

    upload an image, save and view in browser after refresh

--- server/server.js
+++ server/server.js
@@ -12,6 +12,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 
 
 app.use(express.static('static'))
+app.use('/uploads', express.static('uploads'))
 
 app.get('/', (req, res) => {
   res.sendFile('/static/index.html')
